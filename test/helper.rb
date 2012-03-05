886238c8d1e625255b2fa7eca793d2aface9255a
Nicholas E. Rabenau <nerab@gmx.at>
2012-03-05 12:18:45 +0100

    Remove usage of ENV['TMPDIR'] entirely and only use Dir.tmpdir

--- test/helper.rb
+++ test/helper.rb
@@ -24,13 +24,13 @@ module Test
 
       def setup
         @store_file = temp_file_name
-        @store = ::Pwm::Store.new(@store_file, store_password) 
+        @store = ::Pwm::Store.new(@store_file, store_password)
       end
 
       def teardown
         File.unlink(@store_file)
       end
-  
+
       def store_password
         's3cret'
       end
@@ -38,7 +38,7 @@ module Test
       # Make up a name of a file that does not exist in ENV['TMPDIR'] yet
       def temp_file_name
         begin
-          result = File.join(ENV['TMPDIR'] || Dir.tmpdir, "#{self.class.name}-#{Random.rand}.pstore")
+          result = File.join(Dir.tmpdir, "#{self.class.name}-#{Random.rand}.pstore")
         end while File.exists?(result)
         result
       end
@@ -48,21 +48,21 @@ module Test
       APP = 'bin/pwm'
 
       protected
-      
+
       def assert_successful(expected_out, cmd)
         out, err, rc = execute(cmd)
         assert_equal(0, rc.exitstatus, "Expected exit status 0, but it was #{rc.exitstatus}")
         assert(err.empty?, "Expected empty STDERR, but it yielded #{err}")
         assert(out =~ /#{expected_out}/, "'#{out}' did not match expected response '#{expected_out}'")
       end
-  
+
       def assert_error(expected_err, cmd)
         out, err, rc = execute(cmd)
         assert_equal(1, rc.exitstatus, "Expected exit status 1, but it was #{rc.exitstatus}")
         assert(out.empty?, "Expected empty STDOUT, but it yielded #{out}")
         assert(err =~ /#{expected_err}/, "'#{err}' did not match expected response '#{expected_err}'")
       end
-  
+
       def execute(cmd)
         Open3.capture3("echo #{store_password} | #{APP} #{cmd} --file #{store_file}")
       end
