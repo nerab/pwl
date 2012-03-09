74a2ad270b7346847b2edddb87e0d68a9b7affdb
Nicholas E. Rabenau <nerab@gmx.net>
2012-03-09 22:55:54 +0100

    Complete delete command. Closes #9.

---
+++ test/acceptance/test_delete.rb
@@ -0,0 +1,14 @@
+require 'helper'
+
+# Tests `pwm delete`
+class TestDelete < Test::Pwm::AppTestCase
+  def test_delete_blank_key
+    assert_error('may not be blank', 'delete')
+  end
+
+  def test_delete_simple
+    assert_successful('', 'put foo bar')
+    assert_successful('', 'delete foo')
+    assert_error('No entry was found for foo', 'get foo')
+  end
+end
